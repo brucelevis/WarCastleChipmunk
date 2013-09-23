c1af320946a8240727f397b7971ee5b6f3253f90
apple <apple@apple-MacBook-Air.local>
2013-09-23 22:38:17 +0800

    Initial Commit

---
+++ Angela/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AngelaAppController.h
+//  Angela
+//
+//  Created by apple on 13-9-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
